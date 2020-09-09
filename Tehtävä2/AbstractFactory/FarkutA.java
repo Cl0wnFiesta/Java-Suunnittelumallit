d35884036dbb9d26acf12278fca43570f7bc80f2
Cl0wnFiesta <clownfiesta98@gmail.com>
2020-09-09 22:00:00 +0530

    Aal done

--- Tehtävä2/AbstractFactory/FarkutA.java
+++ Tehtävä2/AbstractFactory/FarkutA.java
@@ -5,7 +5,7 @@ import AbstractFactory.interfaces.Farkut;
 public class FarkutA implements Farkut{
 
     @Override
-    public String toString(){ 
+    public String toString(){ // toString() method is used to print the object
         return "Addun Farkut";
     }
 }
