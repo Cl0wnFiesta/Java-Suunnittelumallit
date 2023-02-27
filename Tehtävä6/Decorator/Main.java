3aa788f4e645bd26a03190b2ad07d989539f97a9
Cl0wnFiesta <clownfiesta98@gmail.com>
2023-02-27 22:38:16 +0200

    Decorator done

--- Tehtävä6/Decorator/Main.java
+++ Tehtävä6/Decorator/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Create a new instance of the Data class
+        // Create a new instance of the Data class using decorator
+
         IData data = new SecureData(new FileData());
-         
+
         // Set the data
+
         data.setData("This text is so secure, that no one can read this", "file.txt");
 
         // Read the data
@@ -16,12 +18,20 @@ public class Main {
         IData data2 = new NonSecureData(new FileData());
 
         // Set the data
-     
+
         data2.setData("Wow everyone can read this", "This_file_is_not_secure_at_all.txt");
 
         // Read the data
-        
+
         System.out.println(data2.readData("This_file_is_not_secure_at_all.txt"));
+
+        // Create a new instance of the Data class
+
+        IData data3 = new FileData();
+
+        data3.setData("This is a normal file", "normalFile.txt");
+
+        System.out.println(data3.readData("normalFile.txt"));
 
     }
 }
