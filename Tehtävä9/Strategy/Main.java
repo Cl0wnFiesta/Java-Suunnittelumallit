91fc9d39d6352758d4dbcab23947ad229171c3ef
Cl0wnFiesta <clownfiesta98@gmail.com>
2023-03-06 18:47:21 +0200

    Strategy

--- Tehtävä9/Strategy/Main.java
+++ Tehtävä9/Strategy/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -8,29 +9,32 @@ public class Main {
 
         // create a new int array of length 100000
         int[] randomInts = new int[100000];
-   
-
-        // fill the array with random integers between 0 and 999999 (inclusive)
+       
+        // fill the array with random integers between 1 and 100000 (inclusive)
         for (int i = 0; i < randomInts.length; i++) {
-            randomInts[i] = random.nextInt(100000);
+            randomInts[i] = random.nextInt(999999) + 1;
         }
 
+        int[] randomIntsBubble = Arrays.copyOf(randomInts, randomInts.length);
+        int[] randomIntsInsertion = Arrays.copyOf(randomInts, randomInts.length);
+        int[] randomIntsSelection = Arrays.copyOf(randomInts, randomInts.length);
+    
         Sort sort = new Sort(new BubbleSort());
-        sort.sort(randomInts);
+        sort.sort(randomIntsBubble);
 
         System.out.println();
         sort.setSorting(new InsertionSort());
-        sort.sort(randomInts);
+        sort.sort(randomIntsInsertion);
 
         System.out.println();
         sort.setSorting(new SelectionSort());
-        sort.sort(randomInts);
+        sort.sort(randomIntsSelection);
 
-        // BubbleSort took 9974 milliseconds to sort the array.
+        // BubbleSort took 10095 milliseconds to sort the array.
 
-        // InsertionSort took 737 milliseconds to sort the array.
+        // InsertionSort took 2112 milliseconds to sort the array.
 
-        // SelectionSort took 646 milliseconds to sort the array.
+        // SelectionSort took 1464 milliseconds to sort the array.
 
     }
 }
